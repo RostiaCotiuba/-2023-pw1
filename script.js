0ba1afd9b0c7753fe6afe540870f5f27d254c999
RostiaCotiuba <72990727+RostiaCotiuba@users.noreply.github.com>
2023-05-25 16:21:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,59 @@
-// Массив для хранения задач
-let tasks = [];
+// Получаем элементы из DOM
+const taskInput = document.getElementById('taskInput');
+const taskDescription = document.getElementById('taskDescription');
+const taskPriority = document.getElementById('taskPriority');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
 
-// Функция для добавления задачи
+// Обработчик события для добавления задачи
 function addTask() {
-  const taskNameInput = document.getElementById('task-name');
-  const taskDescriptionInput = document.getElementById('task-description');
-  const taskPrioritySelect = document.getElementById('task-priority');
-
-  // Получаем значения полей формы
-  const taskName = taskNameInput.value;
-  const taskDescription = taskDescriptionInput.value;
-  const taskPriority = taskPrioritySelect.value;
-
-  // Создаем объект задачи
-  const task = {
-    name: taskName,
-    description: taskDescription,
-    priority: taskPriority
-  };
-
-  // Добавляем задачу в массив
-  tasks.push(task);
-
-  // Очищаем поля формы
-  taskNameInput.value = '';
-  taskDescriptionInput.value = '';
-
-  // Обновляем список задач
-  updateTaskList();
-}
-
-// Функция для обновления списка задач
-function updateTaskList() {
-  const taskList = document.getElementById('tasks');
-  const filterButtons = document.getElementsByClassName('filter-btn');
-
-  // Очищаем список задач
-  taskList.innerHTML = '';
-
-  // Фильтруем задачи по выбранному приоритету
-  const filteredTasks = tasks.filter(task => {
-    const activeFilter = document.querySelector('.filter-btn.active');
-    const priorityFilter = activeFilter ? activeFilter.dataset.priority : 'all';
-    return priorityFilter === 'all' || task.priority === priorityFilter;
-  });
-
-  // Создаем элементы задач и добавляем их в список
-  filteredTasks.forEach(task => {
-    const taskItem = document.createElement('li');
-    taskItem.classList.add('task-item');
-
-    const taskName = document.createElement('div');
-    taskName.classList.add('task-name');
-    taskName.textContent = task.name;
-
-    const taskPriority = document.createElement('div');
-    taskPriority.classList.add('task-priority');
-    taskPriority.textContent = task.priority;
-
-    taskItem.appendChild(taskName);
-    taskItem.appendChild(taskPriority);
-    taskList.appendChild(taskItem);
-  });
-
-  // Обновляем активный класс для кнопок фильтрации
-  Array.from(filterButtons).forEach(button => {
-    button.classList.toggle('active', button.dataset.priority === filter);
-  });
-}
-
-// Функция для обработки клика на кнопке фильтрации
-function handleFilterClick(event) {
-  const activeButton = document.querySelector('.filter-btn.active');
-  if (activeButton) {
-    activeButton.classList.remove('active');
+  const taskText = taskInput.value;
+  const taskDesc = taskDescription.value;
+  const taskPrio = taskPriority.value;
+  
+  if (taskText === '') {
+    alert('Пожалуйста, введите задачу.');
+    return;
   }
-  event.target.classList.add('active');
-  updateTaskList();
+  
+  const taskItem = document.createElement('li');
+  taskItem.className = 'taskItem';
+  
+  const taskHeader = document.createElement('div');
+  taskHeader.className = 'taskHeader';
+  
+  const taskTitle = document.createElement('span');
+  taskTitle.className = 'taskTitle';
+  taskTitle.innerText = taskText;
+  
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'deleteButton';
+  deleteButton.innerText = 'Удалить';
+  deleteButton.addEventListener('click', function() {
+    taskList.removeChild(taskItem);
+  });
+  
+  taskHeader.appendChild(taskTitle);
+  taskHeader.appendChild(deleteButton);
+  
+  const taskDescElement = document.createElement('div');
+  taskDescElement.className = 'taskDescription';
+  taskDescElement.innerText = taskDesc;
+  
+  const taskPrioElement = document.createElement('div');
+  taskPrioElement.className = 'taskPriority';
+  taskPrioElement.innerText = `Приоритет: ${taskPrio}`;
+  
+  taskItem.appendChild(taskHeader);
+  taskItem.appendChild(taskDescElement);
+  taskItem.appendChild(taskPrioElement);
+  
+  taskList.appendChild(taskItem);
+  
+  // Очищаем поля ввода задачи
+  taskInput.value = '';
+  taskDescription.value = '';
 }
 
-// Назначаем обработчик события для кнопки добавления задачи
-const addTaskBtn = document.getElementById('add-task-btn');
-addTaskBtn.addEventListener('click', addTask);
-
-// Назначаем обработчик события для кнопок фильтрации
-const filterButtons = document.getElementsByClassName('filter-btn');
-Array.from(filterButtons).forEach(button => {
-  button.addEventListener('click', handleFilterClick);
-});
+// Добавляем обработчик события для нажатия кнопки "Добавить задачу"
+addTaskButton.addEventListener('click', addTask);
